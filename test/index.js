c3c77fcca349b5ab5557aa218fa57f7602a45041
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-30 21:36:15 -0400

    tests now work in the browser

--- test/index.js
+++ test/index.js
@@ -5,10 +5,10 @@ var fs = require('fs');
 var test = require('tape')
 var fncomment = require('../')
 
-function check(t, fixture, lineno, comment) {
+function check(t, fixture, src, lineno, comment) {
   if (Array.isArray(comment)) comment = comment.join('\n');
 
-  var src = fs.readFileSync(__dirname + '/fixtures/' + fixture + '.js', 'utf8');
+  // TODO: in order to work with testling, we need to hard code all fixture paths
   var res = fncomment(src, lineno);
 //  console.error(res.split('\n'));
   t.equal(res, comment, fixture)
@@ -16,11 +16,15 @@ function check(t, fixture, lineno, comment) {
 
 // NOTE: linenos are 1 based since that is how esprima returns line locations as well
 test('\nline comment(s) right above function', function (t) {
-  check(t, 'line-comment-single', 7, '// this is the comment to find');
+  var src;
+  src = fs.readFileSync(__dirname + '/fixtures/line-comment-single.js', 'utf8');
+  check(t, 'line-comment-single', src , 7, '// this is the comment to find');
 
+  src = fs.readFileSync(__dirname + '/fixtures/line-comment-multi.js', 'utf8');
   check(
       t
     , 'line-comment-multi'
+    , src
     , 8
     , [ '// this is the comment to find',
         '// needs to find this line as well',
@@ -30,20 +34,27 @@ test('\nline comment(s) right above function', function (t) {
 })
 
 test('\nline comment(s) above function with empty lines in between', function (t) {
-  check(t, 'line-comment-empty-lines-single', 9, '// this is the comment to find');
+  var src;
 
+  src = fs.readFileSync(__dirname + '/fixtures/line-comment-empty-lines-single.js', 'utf8');
+  check(t, 'line-comment-empty-lines-single', src, 9, '// this is the comment to find');
+
+  src = fs.readFileSync(__dirname + '/fixtures/line-comment-empty-lines-multi.js', 'utf8');
   check(
       t
     , 'line-comment-empty-lines-multi'
+    , src
     , 9
     , [ '// this is the comment to find',
         '// needs to find this line as well',
         '// and this one' ]
   );
 
+  src = fs.readFileSync(__dirname + '/fixtures/line-comment-scattered.js', 'utf8');
   check(
       t
     , 'line-comment-scattered'
+    , src
     , 12
     , [ '// this is the comment to find',
         '',
